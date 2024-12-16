82c7a161979809a1fa8eb511f40a20e24cda54a0
Marcus Cavalcanti <marcus.cavalcanti20@gmail.com>
2024-12-15 22:06:31 -0300

    chore: layouts flow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,38 @@
-import { ReactNode } from 'react'
 import '../styles/globals.css'
+
+import localFont from 'next/font/local'
+
+const articulat = localFont({
+  variable: '--font-articulat',
+  src: [
+    {
+      path: '../fonts/ArticulatCF-Normal.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/ArticulatCF-Medium.otf',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+})
 
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return children
+export default async function RootLayout({
+  params,
+  children,
+}: {
+  params: Promise<{ locale: string }>
+  children: React.ReactElement
+}) {
+  const { locale } = await params
+  return (
+    <html lang={locale ?? 'en'} className={articulat.variable}>
+      <body className="bg-black font-medium text-msgray-300 overflow-x-hidden">
+        {children}
+      </body>
+    </html>
+  )
 }
